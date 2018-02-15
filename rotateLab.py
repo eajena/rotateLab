2e0e35ae5510a2a55cfbf38e4932b57e56f32445
ab <ab@elon>
2018-02-15 13:32:42 +0100

    add std of a and b for measure of colorfulness

--- rotateLab.py
+++ rotateLab.py
@@ -1,9 +1,12 @@
-import sys
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import sys, os, getopt, glob
 import numpy as np
 import matplotlib.pyplot as plt
+
 from skimage import io
-from skimage.color import rgb2gray, rgb2lab, lab2rgb
-from skimage.util import img_as_uint
+from skimage.color import rgb2lab, lab2rgb
 
 
 def do_rot(lab, theta):
@@ -21,7 +24,7 @@ def do_rot(lab, theta):
     result[:,:,0] = lab[:,:,0]
     result[:,:,1] = r[:,0].reshape(h,w)
     result[:,:,2] = r[:,1].reshape(h,w)
-    if False:
+    if option["verbose"]:
         plt.scatter(lab[:,:,1],lab[:,:,2], color="blue")
         plt.scatter(result[:,:,1],result[:,:,2], color="green")
         plt.axes().set_aspect('equal')
@@ -29,28 +32,53 @@ def do_rot(lab, theta):
     return result
 
 
+def all_files(in_dir, out_dir, rotate):
+    for f in glob.glob('%s/*' % in_dir):
+        img = io.imread(f)
+        lab = rgb2lab(img)
+        if rotate is None:
+            #mask = np.logical_and(25.<lab[:,:,0], lab[:,:,0]<75.)
+            #plt.figure(1),plt.imshow(lab[:,:,0]), plt.colorbar()
+            #plt.figure(2),plt.imshow(mask)
+            #plt.show()
+            #m = np.std(lab[:,:,1][mask]) * np.std(lab[:,:,2][mask])
+            m = np.std(lab[:,:,1]) * np.std(lab[:,:,2])
+            print m, f
+            io.imsave("%s/%.4f-%s" % (out_dir, m, os.path.basename(f)), img)
+        else:
+            r = lab2rgb(do_rot(lab, rotate))
+            io.imsave("%s/%s" % (out_dir, os.path.basename(f)), r)
+
+
+def read_options():
+  try:
+    opts, args = getopt.getopt(sys.argv[1:], 'ar:i:o:',
+                        ["analyse","rotate=","in=","out="])
+  except getopt.GetoptError as err:
+    leave(str(err))
+
+  for o, a in opts:
+    if   o in ('-a',"--analyze"): option["rotate"] = None
+    elif o in ("-r","--rotate"):  option["rotate"] = float(a)
+    elif o in ("-i","--in"):      option["in"] = a
+    elif o in ('-o',"--out"):     option["out"] = a
+    elif o in ('-v',"--verbose"): option["verbose"] = True
+    else: assert False, "unhandled option: %s" % str(o)
+
+    assert os.path.isdir(option["in"]), "No such directory: %s" % option["in"]
+    assert os.path.isdir(option["out"]), "No such directory: %s" % option["out"]
+
 
 if __name__ == "__main__":
-    if len(sys.argv)==4:
-        img = io.imread(sys.argv[1])
-        lab = rgb2lab(img)
-        r = do_rot(lab, float(sys.argv[3]))
-        io.imsave(sys.argv[2], lab2rgb(r))
+    option = {
+        "verbose" : False,
+        "rotate"  : None,
+        "in"      : "in",
+        "out"     : "out"
+    }
+    read_options()
+
+    all_files(option["in"], option["out"], option["rotate"])
+
         #plt.imshow(lab2rgb(r))
         #plt.show()
-
-    if False:
-        img = io.imread(sys.argv[1])
-        plt.imshow(img)
-        lab = rgb2lab(img)
-
-        r1 = do_rot(lab, -180)
-        r2 = do_rot(r1,  90)
-        r3 = do_rot(r2,  90)
-        restored = lab2rgb(lab)
-
-        plt.subplot(221),plt.imshow(lab2rgb(lab))
-        plt.subplot(222),plt.imshow(lab2rgb(r1))
-        plt.subplot(223),plt.imshow(lab2rgb(r2))
-        plt.subplot(224),plt.imshow(lab2rgb(r3))
-        plt.show()
